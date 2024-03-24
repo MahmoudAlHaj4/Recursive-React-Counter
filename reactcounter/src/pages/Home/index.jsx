90ce9d6ee72fb27d945494e6757ff382163cdea0
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-03-24 23:44:28 +0200

    add style file and counter

--- reactcounter/src/pages/Home/index.jsx
+++ reactcounter/src/pages/Home/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import "./style.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="counter">
+        <p>-1</p>
+        <button >+</button>
+        <button>-</button>
+    </div>
+  );
 };
 
 export default Home;

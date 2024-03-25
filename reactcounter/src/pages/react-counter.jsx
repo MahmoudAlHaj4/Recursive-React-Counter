5189aa455bd034de5165ed8de98ca0cff20fba07
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-03-25 21:53:34 +0200

    remove check functions and updatee usestate

--- reactcounter/src/pages/react-counter.jsx
+++ reactcounter/src/pages/react-counter.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { useState} from "react";
 
-const AddBtn = ()=>{
-    const [nested,setnested] = useState()
+const AddBtn = ({initial})=>{
+    const [nested,setnested] = useState([initial])
 
     const checkadd = ()=>{
         console.log("clicked")

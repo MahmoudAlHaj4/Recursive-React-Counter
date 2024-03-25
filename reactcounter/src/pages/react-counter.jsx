ff6d05020368912e1a0603046f87ea790493df7c
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-03-25 21:53:57 +0200

    add switch case

--- reactcounter/src/pages/react-counter.jsx
+++ reactcounter/src/pages/react-counter.jsx
@@ -4,9 +4,19 @@ import { useState} from "react";
 const AddBtn = ({initial})=>{
     const [nested,setnested] = useState([initial])
 
-    const checkadd = ()=>{
-        console.log("clicked")
-    }
+
+    const addRemove = (operation)=>{
+        switch(operation){
+            case "+":
+            setnested([...nested, initial])
+            break;
+
+            case "-":
+                setnested([])
+                break;
+
+            default:
+        }
     const checkRemove = ()=>{
         console.log("checked 2")
     }

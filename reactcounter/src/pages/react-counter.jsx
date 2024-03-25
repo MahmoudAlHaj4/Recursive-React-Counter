0488bc98e3925fb4e3c5399c4eb8cca0c69c6455
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-03-25 21:19:02 +0200

    add functions to check buttons

--- reactcounter/src/pages/react-counter.jsx
+++ reactcounter/src/pages/react-counter.jsx
@@ -3,6 +3,13 @@ import { useState} from "react";
 
 const AddBtn = ()=>{
     const [nested,setnested] = useState()
+
+    const checkadd = ()=>{
+        console.log("clicked")
+    }
+    const checkRemove = ()=>{
+        console.log("checked 2")
+    }
     return (
         <div>
             - <button>+</button>

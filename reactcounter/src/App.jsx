d09b6b5662d02a0a0aca31bbade3a4cff1dce197
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-03-25 21:08:53 +0200

    update app

--- reactcounter/src/App.jsx
+++ reactcounter/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import AddBtn from "./pages/react-counter";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <Routes>
         <Route path ="/" element= {<Home />}/>
+        <Route path ="reactcounter" element= {<AddBtn />}/>
       </Routes>
       </BrowserRouter>
     </div>

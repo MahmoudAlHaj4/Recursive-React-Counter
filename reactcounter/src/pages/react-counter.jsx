22720bc0a8e5a1528b8fce82983593b4b31fd245
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-03-25 21:16:52 +0200

    add buttons and usestate

--- reactcounter/src/pages/react-counter.jsx
+++ reactcounter/src/pages/react-counter.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import { useState} from "react";
 
 const AddBtn = ()=>{
+    const [nested,setnested] = useState()
     return (
-        <div>Heloo</div>
+        <div>
+            - <button>+</button>
+                <button>-</button>
+        </div>
 
     )
 }
